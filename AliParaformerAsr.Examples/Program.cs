a81978926a7cb4bc927960f898004baa30b1a8cc
manyeyes <zx.520.521@126.com>
2025-08-06 13:02:46 +0800

    update examples

--- AliParaformerAsr.Examples/Program.cs
+++ AliParaformerAsr.Examples/Program.cs
@@ -1,6 +1,10 @@
-﻿using AliParaformerAsr;
-using NAudio.Wave;
-
+﻿// See https://github.com/manyeyes for more information
+// Copyright (c)  2023 by manyeyes
+/*
+ * Before running, please prepare the model first
+ * Model Download:
+ * Please read README.md
+ */
 namespace AliParaformerAsr.Examples;
 internal static partial class Program
 {
@@ -8,7 +12,13 @@ internal static partial class Program
     [STAThread]
     private static void Main()
     {
-        OfflineRecognizer();
-        OnlineRecognizer();
+        string defaultOfflineModelName = "aliparaformerasr-large-zh-en-timestamp-onnx-offline2";
+        OfflineRecognizer(streamDecodeMethod: "one", modelName: defaultOfflineModelName);
+        string defaultOnlineModelName = "aliparaformerasr-large-zh-en-onnx-online";
+        OnlineRecognizer(streamDecodeMethod: "one", modelName: defaultOnlineModelName);
+        //suggest GC recycling (non mandatory)
+        GC.Collect(); //trigger recycling
+        GC.WaitForPendingFinalizers(); //waiting for the terminator to complete execution
+        GC.Collect(); //recycling again
     }
 }
